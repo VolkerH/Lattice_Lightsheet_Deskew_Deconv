310669bb23bef743963ada0053e70a1278b79e6f
Volker Hilsenstein <volker.hilsenstein@gmail.com>
2019-03-27 11:20:37 +1100

    commiting omitted file

--- lls_dd/settings.py
+++ lls_dd/settings.py
@@ -1,7 +1,23 @@
 import json
 from typing import Dict, Any
 
+
 def create_fixed_settings(file: str):
+    """creates a minimalist .json file with a dictionary fixed settings
+    
+    Parameters
+    ----------
+    file : str
+            path to the file
+
+    Notes
+    -----
+    the fixed settings file contains settings pertaining to 
+    a particular microscope that are known not to change between
+    experiments. The settings below are specific to our lls at Monash
+    but probably apply to most lls microscopes based on the original
+    Janelia design.
+    """
     fs = {}
     fs["xypixelsize"] = 0.1040
     fs["angle_fixed"] = 31.8
@@ -12,8 +28,17 @@ def create_fixed_settings(file: str):
 
 
 def read_fixed_settings(file: str) -> Dict[str, Any]:
+    """reads a fixed settings .json file and returns the settings as a dictionary
+    
+    Parameters
+    ----------
+    file : str
+            Path to fixed settings file
+    
+    Returns
+    -------
+    Dict[str, Any]
+            settings dictionary
+    """
     with open(file, "r") as fp:
         return json.load(fp)
-
-# create_fixed_settings("fixed_settings.json")
-# read_fixed_settings("fixed_settings.json")

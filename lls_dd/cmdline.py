ea214c0c2bb4a998944739893b54bad3b8626f53
Volker Hilsenstein <volker.hilsenstein@monash.edu>
2019-12-02 16:28:05 +1100

    fixed docu for cmdline option -s

--- lls_dd/cmdline.py
+++ lls_dd/cmdline.py
@@ -72,9 +72,8 @@ def cli(ctx, exp_folder, home, debug, fixed_settings):
     "--decon-deskew",
     is_flag=True,
     default=False,
-    help="if  deconvolution was chosen, rotate deconvolved "
-    "and deskewed data to coverslip coordinates"
-    " and save.",
+    help="if  deconvolution was chosen, deskew the deconvolved "
+    " data and save.",
 )
 @click.option(
     "-n",
